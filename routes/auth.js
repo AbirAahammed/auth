444c64e8bca0ded93fbf339826e8338ed47ad285
AbirAahammed <ahammed.abir47@gmail.com>
2022-01-28 12:32:15 -0600

    partially done redis access needs to be sorted.

--- routes/auth.js
+++ routes/auth.js
@@ -3,10 +3,25 @@ var router = express.Router();
 var logger = require('morgan');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
-
+const redis = require('redis');
+    
 // get config vars
 dotenv.config();
 
+// db client
+const client = redis.createClient('0.0.0.0', '7000');
+
+client.on('connect', function() {
+  console.log('Connected!!!');
+  client.get('framework', function(err, reply) {
+    console.log(reply); // ReactJS
+  });
+});
+
+
+client.on('error', (err) => {
+  console.log(err);
+});
 
 /**Default Algorithm is used HS256 */
 function generateWebToken(param) {
@@ -19,14 +34,32 @@ function generateWebToken_hs512(param) {
 /* GET a JWT token. */
 router.get('/alltoken', function(req, res, next) {
     console.debug(`User Name : ${req.body['username']}`)
-
     /**Generate the token */
     var token_HS256 = generateWebToken(req.body);
     
     /**Respond object */
     var allToken = {"HS256":token_HS256}
+
+
+
     
     res.send(allToken);
   });
+
+  router.get('/redis', async function (req, res) {
+    await client.connect()
+    await client.hmset('frameworks_hash', 'javascript', 'ReactJS', 'css', 'TailwindCSS', 'node', 'Express');
+
+    client.hgetall('frameworks_hash', function(err, object) {
+      console.log(object); // { javascript: 'ReactJS', css: 'TailwindCSS', node: 'Express' }
+    });
+    client.set('framework', 'ReactJS');
+
+    client.get('framework', function(err, reply) {
+      console.log(reply); // ReactJS
+      res.send(result)
+    });
+})
+
 
   module.exports = router;
